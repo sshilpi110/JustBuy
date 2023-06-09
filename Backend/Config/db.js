1c3ed384d36355199fa4a2d6003e177b84258cea
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-06-09 17:20:26 +0530

    done

--- Backend/Config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const Connection = mongoose.connect(process.env.MONGOATLASURL,
-    {
-        useNewUrlParser: true
-    });
-
-module.exports={Connection};
